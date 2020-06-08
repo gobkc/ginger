ac8a2a8ca7674ea74b568b6fc5daff0bf692b32a
熊建波 <10044375@qq.com>
2020-06-08 10:43:29 +0800

    GIN的启动文件修改

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ginger
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
--- start.go
+++ start.go
@@ -1,1 +1,34 @@
 package ginger
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+type Ginger struct {
+	App *gin.Engine
+}
+
+//默认就是DEBUG。如果设置为FALSE，则ReleaseMode
+func (g *Ginger) Debug(isDebug bool) *Ginger {
+	if !isDebug {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	return g
+}
+
+//加载路由
+func (g *Ginger) LoadRouter(router func(router *gin.Engine) gin.IRoutes) *Ginger {
+	g.App = gin.Default()
+	g.App.Use(Cors())
+	router(g.App)
+	return g
+}
+
+//开启服务
+func (g *Ginger) Start(addr string) {
+	if g.App == nil {
+		log.Fatalf("No Router")
+	}
+	g.App.Run(addr)
+}
